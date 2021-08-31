4fe5a8b2ed7a6d0f4e1074df10ff75663efe3bd6
abegic <amir.begic@namics.com>
2021-08-31 14:59:58 +0200

    performance tuning, removed bold from font

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const svg = document.getElementById('svg-element')
 const warp = new Warp(svg)
 
-//warp.extrapolate(4)
+warp.extrapolate(2)
 warp.transform(([ x, y ]) => [ x, y, y ])
 
 
@@ -9,7 +9,7 @@ let offset = 0
 function animate()
 {
 	warp.transform(([ x, y, oy ]) => [ x, oy + 4 * Math.sin(x / 16 + offset), oy ])
-	offset += 0.06
+	offset += 0.04
 	requestAnimationFrame(animate)
 }
 
